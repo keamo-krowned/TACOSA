155f2606c3007233f662d5abc93de447a60b3f59
kevin <klmir@KEVINSHP>
2026-08-21 17:48:14 +0200

    Added New Page, Added LB on Main

---
+++ LandingPage.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LandingPage.aspx.cs" Inherits="TACOSA.LandingPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="background-color: #FF6600">
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Maintain"></asp:Label>
+    </form>
+</body>
+</html>
--- LandingPage.aspx.cs
+++ LandingPage.aspx.cs
@@ -7,19 +7,9 @@ using System.Web.UI.WebControls;
 
 namespace TACOSA
 {
-    public partial class defualt : System.Web.UI.Page
+    public partial class LandingPage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
