9de8d160b3c0d7c5be4a2fd34d9385f2367e7c78
kevin <klmir@KEVINSHP>
2026-08-26 23:08:01 +0200

    added sql stored procedure

--- TouristRegister.aspx.designer.cs
+++ TouristRegister.aspx.designer.cs
@@ -204,6 +204,15 @@ namespace TACOSA
         protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator11;
 
         /// <summary>
+        /// lblError control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblError;
+
+        /// <summary>
         /// BtnContinue control.
         /// </summary>
         /// <remarks>
