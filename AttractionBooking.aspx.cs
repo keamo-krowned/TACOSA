549a054196004f57414185653d8cec490cbd3a30
kevin <klmir@KEVINSHP>
2026-08-26 22:23:30 +0200

    attr booking in main, Error fixed

--- AttractionBooking.aspx.cs
+++ AttractionBooking.aspx.cs
@@ -14,9 +14,8 @@ namespace TACOSA
 
         }
 
-        protected System.Void Button1_Click(System.Object sender, System.EventArgs e)
+        protected void Button1_Click(object sender, EventArgs e)
         {
-
 
         }
     }
