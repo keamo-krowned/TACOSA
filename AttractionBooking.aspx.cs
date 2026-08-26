0ac6a63cef1a93d010ca85ece7ee5f354cf5d7ef
keamo-krowned <akeamo2408@gmail.com>
2026-08-26 13:52:09 +0200

    26/08 updates

---
+++ AttractionBooking.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TACOSA
+{
+    public partial class AttractionBooking : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(System.Object sender, System.EventArgs e)
+        {
+
+
+        }
+    }
+}
