e93090394df8fcaa55cbc85eb7a811dbd2d78182
Pal Sebola <54864615@mynwu.ac.za>
2026-08-30 20:31:24 +0200

    coded cs file

--- AttractionBooking.aspx.cs
+++ AttractionBooking.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace TACOSA
 {
@@ -12,8 +13,46 @@ namespace TACOSA
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if(!IsPostBack)
+            {
+                LoadAttraction();
+            }
         }
+        priavte void LoadAttraction()
+        {
+            string attractionID = Request.QueryString["id"];
+
+            if(string.IsNullOrEmpty(attractionID)
+            {
+                return;
+            }
+            string connectionString = ConfigurationManager.ConnectionString["TACOSAConnection"].ConnectionString;
+
+            string query = @"SELECT * FROM Attractions WHERE AttractionID = @AttractionID";
+
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@AttractionID", attractionID);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    
+                    if(reader.Read())
+                    {
+                        lblName.Text = reader["Name"].ToString();
+                        lblCategory.Text = reader["Category"].ToString();
+                        Rating.Text = reader["Rating"].ToString();
+                        lbldescription.Text = reader["Description"].ToString();
+                        lblLocation.Text = reader["Location"].ToString();
+                        lblAvailable.Text = reader["Available"].ToString();
+                        Image1.ImageUrl = raeder["ImageURL"].ToString();
+                    }
+                }
+            }
+        }
+
 
         protected void Button1_Click(object sender, EventArgs e)
         {
