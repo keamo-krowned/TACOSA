05d075c06e7a48f3daa650840f0d1e2e6ec75b36
Xongile Baloyi <236609609+Xongile888@users.noreply.github.com>
2026-09-01 13:01:22 +0200

    i removed the SqlDataSource replaced it with code.. pls check mebore merging if it still works on ur side.

--- Attractions.aspx.cs
+++ Attractions.aspx.cs
@@ -203,12 +203,16 @@ namespace TACOSA
 
         protected void btnViewDetails_Click(object sender, EventArgs e)
         {
-           
+
             Button btn = (Button)sender;
 
             string attractionID = btn.CommandArgument;
 
-            Response.Redirect("AttractionBooking.aspx?id=" + attractionID);
+            Session["AttractionID"] = attractionID;
+
+            Response.Cookies["AttractionID"].Value = attractionID;
+
+            Response.Redirect("AttractionBooking.aspx");
         }
     }
 }
